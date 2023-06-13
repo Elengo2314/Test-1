0837a870680a15e7f0762c416f521e3731b981e8
Elengo2314 <elengor2317@gmail.com>
2023-06-13 15:52:11 +0530

    sample project structure

---
+++ Sample.java
@@ -0,0 +1,13 @@
+package org.test.Sample2;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+
+
+	}
+
+}
